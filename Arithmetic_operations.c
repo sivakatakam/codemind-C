aed57ecf26c7711d74e4bf5cb1eb1fdc89582be2
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2022-12-14 07:30:47 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e,f,g;     scanf("%d%d",&a,&b);     c=a+b;     printf("Sum:%d ",c);     d=a-b;     printf("Difference:%d ",d);     e=a*b;     printf("Product:%d ",e);     f=a/b;     printf("Quotient:%d ",f);     g=a%b;     printf("Remainder:%d ",g); }

---
+++ Arithmetic_operations.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,g;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum:%d
+",c);
+    d=a-b;
+    printf("Difference:%d
+",d);
+    e=a*b;
+    printf("Product:%d
+",e);
+    f=a/b;
+    printf("Quotient:%d
+",f);
+    g=a%b;
+    printf("Remainder:%d
+",g);
+}
