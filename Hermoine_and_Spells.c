b721e486f1b841ad762628b557c7e0c188c277f9
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2022-12-23 19:34:58 +0530

    #include<stdio.h> int main() {     int a,b,c,x,y;     scanf("%d%d%d",&a,&b,&c);     if(a>b&&a>c)     {         x=a;         if(b>c)         {            y=b;         }         else         {             y=c;         }     }     else if(b>a&&b>c)     {         x=b;         if(a>c)         {             y=a;         }         else         {             y=c;         }     }     else     {         x=c;         if(a>b)         {             y=a;         }         else         {             y=b;         }     }     printf("%d",x+y); }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,x,y;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    {
+        x=a;
+        if(b>c)
+        {
+           y=b; 
+        }
+        else
+        {
+            y=c;
+        }
+    }
+    else if(b>a&&b>c)
+    {
+        x=b;
+        if(a>c)
+        {
+            y=a;
+        }
+        else
+        {
+            y=c;
+        }
+    }
+    else
+    {
+        x=c;
+        if(a>b)
+        {
+            y=a;
+        }
+        else
+        {
+            y=b;
+        }
+    }
+    printf("%d",x+y);
+}
