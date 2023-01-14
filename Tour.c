7b70232e344160ae7fad5bd9d20f09ab054df436
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-14 07:54:39 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int n,m,c;         scanf("%d%d",&n,&m);         c=n*5+m*7;         printf("%d ",c);     } }

---
+++ Tour.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,m,c;
+        scanf("%d%d",&n,&m);
+        c=n*5+m*7;
+        printf("%d
+",c);
+    }
+}
